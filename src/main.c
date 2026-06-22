f3d482c89e87b22a9b06ea66f92f0c5d03f36fc2
zach grindle <elizabethgodsey@Elizabeths-MacBook-Pro-2.local>
2026-06-22 09:48:27 -0500

    lcd branch independent of bmp/sht init commit

--- src/main.c
+++ src/main.c
@@ -5,21 +5,20 @@
 #include "queue.h"
 #include "sensor.h"
 
-
-static QueueHandle_t xLoggingQueue;
-SemaphoreHandle_t i2c_mutex = NULL;
+static QueueHandle_t xImuQueue;
 
 //look into interrupts for this task when more stuff gets added in
 void ImuTask(void* pv)
 {
    TickType_t xLastWakeTime = xTaskGetTickCount();
-   sensor_message msg;
-   msg.id = SENSOR_IMU;
+   IMUParsed localimutask;
 
     while(1){
-      if(IMU_data_exfil(&msg.IMUParsed))
+      
+      
+      if(IMU_data_exfil(&localimutask))
       {
-        xQueueSend(xLoggingQueue, (void*) &msg.IMUParsed, 0);
+        xQueueSend(xImuQueue, (void*) &localimutask, 0);
       }
       else
       {
@@ -30,100 +29,29 @@ void ImuTask(void* pv)
     }
 }
 
-void LoggingTask(void* pv)
+void ImuLoggingTask(void* pv)
 {
-    sensor_message msg;
-
+    IMUParsed imureceived;
 
     while (1) {
-
-        if (xQueueReceive(xLoggingQueue, &msg, portMAX_DELAY) == pdTRUE)
-        {
-            switch(&msg.id)
-            {
-                case SENSOR_IMU:
-                
-                    printf("ax %f | ay %f | az %f\n", (float)imureceived.ax_parsed, (float)imureceived.ay_parsed, (float)imureceived.az_parsed);
-                    printf("--------------------------------\n");
-                    printf("gx %f | gy %f | gz %f\n", (float)imureceived.gx_parsed, (float)imureceived.gy_parsed, (float)imureceived.gz_parsed);
-                    printf("--------------------------------\n");
-                break;
-                
-
-                case SENSOR_TEMP:
-                    printf("temp: %f\n", msg.sht.parsed_temperature);
-                    printf("--------------------------------\n");
-                    printf("Humidity %f\n", msg.sht.parsed_humidity);
-                    printf("--------------------------------\n"); 
-                break;
-
-            }
-        }
         //blocks until queue is ready
-        
+        if (xQueueReceive(xImuQueue, &imureceived, portMAX_DELAY) == pdTRUE) {
+            printf("ax %f | ay %f | az %f\n", (float)imureceived.ax_parsed, (float)imureceived.ay_parsed, (float)imureceived.az_parsed);
+            printf("--------------------------------\n");
+            printf("gx %f | gy %f | gz %f\n", (float)imureceived.gx_parsed, (float)imureceived.gy_parsed, (float)imureceived.gz_parsed);
+            
+        }
 
     }
 }
-
-void tempTask(void* pv)
-{
-    TickType_t xLastWakeTime = xTaskGetTickCount();
-    sensor_message msg;
-    msg.id = SENSOR_TEMP;
-    
-    while(1)
-    {
-        if(temp_exfil(&msg.sht))
-        {
-            xQueueSend(xLoggingQueue, (void*) &msg.sht, 0);
-        }
-
-        else{
-            printf("SHT-31 read error\n");
-        }
-
-
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(3000)); // HEAVY DELAY TIME DO TO THE WANT OF ACCURATE TEMP READINGS DUE TO TEMP CHIP HEATUP
-    }
-}
-
-void baroTask(void* pv)
-{
-    TickType_t xLastWakeTime = xTaskGetTickCount();
-    sensor_message msg;
-    msg.id = SENSOR_BARO;
-    
-    while(1)
-    {
-        //if(baroexfil(//?)) //need to put somethong like the bmp struct to fit the parameter but then a variable that reads just the pressure
-        
-            xQueueSend(xLoggingQueue, (void*) , 0);
-        }
-
-        else{
-            printf("baro bmp280 read error\n");
-        }
-
-
-    vTaskDelayUntil(&xLastWakeTime, pdMS_TO_TICKS(1000)); // HEAVY DELAY TIME DO TO THE WANT OF ACCURATE TEMP READINGS DUE TO TEMP CHIP HEATUP
-}
-
-
-
 
 
 int main()
 {
      stdio_init_all();
      sleep_ms(2000);
-    i2c_mutex = xSemaphoreCreateMutex(); // need mutex for the sht and bmp since they share the same bus
-    
     configure_i2c_peripheral();
-    
-   
-    xloggingQueue = xQueueCreate(16, sizeof(sensor_message));
-    
-    
+    xImuQueue = xQueueCreate(16, sizeof(IMUParsed));
     xTaskCreate(ImuTask,
                 "imu task",
                 256,
@@ -132,25 +60,9 @@ int main()
                 NULL
     );
 
-     xTaskCreate(LoggingTask,
+     xTaskCreate(ImuLoggingTask,
                 "logging task",
                 512,
-                NULL,
-                4,
-                NULL
-    );
-
-     xTaskCreate(tempTask,
-                "temp task",
-                256,
-                NULL,
-                3,
-                NULL
-    );
-
-      xTaskCreate(baroTask,
-                "temp task",
-                256,
                 NULL,
                 2,
                 NULL
--- src/sensor.h
+++ src/sensor.h
@@ -2,24 +2,14 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
-
 #define PERIPHERAL_ADDRESS 0x6B
 #define IMU_DATA_REG 0x35
 #define IMU_CONFIG 0x22
-
-#define SHT_PERIPHERAL 0x44
-
-
-
 #define ACCEL_SCALE 4096.0f
 #define GYRO_SCALE 65.536f
-
 #define I2C_SPEED_STANDARD 100000
 #define I2C_SDA_PIN 6
 #define I2C_SCL_PIN 7
-//need to declare pins for the otherr imu bus
-
-
 
 typedef struct
 {
@@ -31,47 +21,7 @@ typedef struct
     float gz_parsed;
 }IMUParsed;
 
-//state machine for the logging, each message will have an id with its state so it can be logged properly
-typedef enum
-{
-    SENSOR_IMU,
-    SENSOR_BARO,
-    SENSOR_TEMP
-}sensor_id;
-
-typedef struct 
-{
-    float parsed_temperature;
-    float parsed_humidity;
-}SHT_31;
-
-
-
-typedef struct{
-    sensor_id id,
-        union{
-            IMUParsed imu;
-            SHT_31 sht;
-            uint32_t pressure; //for the bmp280
-        }logging_payload;
-}sensor_message;
-
-
-
-
-
 void configure_i2c_peripheral();
 bool IMU_data_exfil(IMUParsed *parsed_data);
 void ImuTask(void* pv);
-void LoggingTask(void* pv);
-
-void BaroTask(void* pv);
-void tempTask(void pv);
-void baro_exfil(uint32_t pressure);
-void temp_exfil(SHT_31 *sht);
-
-
-
-
-
-
+void ImuLoggingTask(void* pv);
